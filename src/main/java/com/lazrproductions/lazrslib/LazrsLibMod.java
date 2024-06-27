4e7f9aad898926bd5b0431f8c1ec7350a24754a6
LazrProductions <102117526+LazrProductions@users.noreply.github.com>
2024-06-26 23:38:51 -0400

    FEAT: update 1.1.0

--- src/main/java/com/lazrproductions/lazrslib/LazrsLibMod.java
+++ src/main/java/com/lazrproductions/lazrslib/LazrsLibMod.java
@@ -11,7 +11,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-// The value e should match an entry in the META-INF/mods.toml file
 @Mod(LazrsLibMod.MODID)
 public class LazrsLibMod {
     public static final Logger LOGGER = LogManager.getLogger(LazrsLibMod.MODID);
---
+++ src/main/java/com/lazrproductions/lazrslib/common/config/ConfigSide.java
@@ -0,0 +1,7 @@
+package com.lazrproductions.lazrslib.common.config;
+
+public enum ConfigSide {
+    COMMON,
+    CLIENT,
+    SERVER
+}
