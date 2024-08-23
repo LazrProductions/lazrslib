2636149ea9f70f6bae03e63c32b7a22853bc0625
LazrProductions <102117526+LazrProductions@users.noreply.github.com>
2024-08-22 20:20:56 -0400

    FIX: 1.1.1

--- src/main/java/com/lazrproductions/lazrslib/common/tag/TagUtilities.java
+++ src/main/java/com/lazrproductions/lazrslib/common/tag/TagUtilities.java
@@ -1,7 +1,10 @@
 package com.lazrproductions.lazrslib.common.tag;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.item.ItemStack;
 
 public class TagUtilities {
     public static final String TAG_POSITION = "Position";
@@ -17,5 +20,22 @@ public class TagUtilities {
         CompoundTag compoundtag1 = new CompoundTag();
         compoundtag1.putIntArray(TAG_POSITION, new int[] { pos.getX(), pos.getY(), pos.getZ() });
         return compoundtag1;
+    }
+
+     public static NonNullList<ItemStack> fromTag(ListTag tag) {
+        NonNullList<ItemStack> items = NonNullList.withSize(tag.size(), ItemStack.EMPTY);
+        for (int i = 0; i < tag.size(); i++) {
+            items.set(i, ItemStack.of(tag.getCompound(i)));
+        } 
+        return items;
+    }
+    public static ListTag toTag(NonNullList<ItemStack> items) {
+        ListTag list = new ListTag();
+        items.forEach((c) -> {
+            CompoundTag t = new CompoundTag();
+            c.save(t); 
+            list.add(t);
+        });
+        return list;
     }
 }
