5c8cf712c794519c861d3d5427ccdeadf5813215
LazrProductions <102117526+LazrProductions@users.noreply.github.com>
2024-08-22 20:25:47 -0400

    FIX: 1.1.1

--- src/main/java/com/lazrproductions/lazrslib/common/network/packet/ParameterizedLazrPacket.java
+++ src/main/java/com/lazrproductions/lazrslib/common/network/packet/ParameterizedLazrPacket.java
@@ -2,24 +2,20 @@ package com.lazrproductions.lazrslib.common.network.packet;
 
 import java.util.function.Supplier;
 
-import javax.annotation.Nonnull;
-
-import com.lazrproductions.lazrslib.LazrsLibMod;
 import com.lazrproductions.lazrslib.common.network.LazrNetworkedParameterHandler;
 import com.lazrproductions.lazrslib.common.network.base.ILazrPacket;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.fmllegacy.network.NetworkDirection;
 import net.minecraftforge.fmllegacy.network.NetworkEvent;
+import net.minecraftforge.fmllegacy.network.NetworkEvent.Context;
 
 public abstract class ParameterizedLazrPacket implements ILazrPacket {
 
     Object[] parameters;
 
     public ParameterizedLazrPacket(FriendlyByteBuf buffer) {
-        read(buffer);
+        LazrNetworkedParameterHandler.readPacket(this, buffer);
     }
     public ParameterizedLazrPacket(Object... p) {
         this.parameters = p;
@@ -30,10 +26,6 @@ public abstract class ParameterizedLazrPacket implements ILazrPacket {
         LazrNetworkedParameterHandler.writePacketParameters(parameters, buffer);
     }
 
-    @Override
-    public void read(FriendlyByteBuf buffer) {
-        LazrNetworkedParameterHandler.readPacket(this, buffer);
-    }
 
     public void setFrom(Object[] parameters) {
         this.parameters = parameters;
@@ -41,23 +33,20 @@ public abstract class ParameterizedLazrPacket implements ILazrPacket {
     }
     public abstract void loadValues(Object[] parameters);
 
-    @Override
-    public void handle(Supplier<NetworkEvent.Context> context) {
+    public void handle(Supplier<Context> context) {
         NetworkEvent.Context ctx = context.get();
         ctx.enqueueWork(() -> {
-            Minecraft inst = Minecraft.getInstance();
-            Player player = context.get().getSender() == null ? inst.player : context.get().getSender();
-            if(player != null)
-                if(player.level.isClientSide())
-                    handleClientside(player);
-                else
-                    handleServerside((ServerPlayer)player);
+            NetworkDirection dir = ctx.getDirection();
+            if(dir == NetworkDirection.PLAY_TO_SERVER || dir == NetworkDirection.LOGIN_TO_SERVER)
+                handleServerside(context);
             else
-                LazrsLibMod.LOGGER.error("Couldn't execute parameterized packet because player is null.");
+                handleClientside(context);
         });
         ctx.setPacketHandled(true);
     }
 
-    public abstract void handleClientside(@Nonnull Player player);
-    public abstract void handleServerside(@Nonnull ServerPlayer player);
+
+    public abstract void handleClientside(Supplier<Context> supplier);
+    public abstract void handleServerside(Supplier<Context> supplier);
+
 }
